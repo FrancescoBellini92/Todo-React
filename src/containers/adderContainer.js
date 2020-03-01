46b3d9ab2ef06f613dcd3fec4a78a6082a0e9317
Francesco Bellini <francescobellini92@gmail.com>
2020-03-01 22:57:11 +0100

    Delete adderContainer.js

--- src/containers/adderContainer.js
+++
@@ -1,6 +0,0 @@
-import Adder from '../components/Adder';
-import { connect } from 'react-redux';
-import { addTodo, addList } from '../actions/index';
-
-export const TodoAdderContainer = connect(null, {addFunc: addTodo})(Adder);
-export const ListAdderContainer = connect(null, {addFunc: addList})(Adder);
